81e5966355e93d47a949998c0fd43c16dcbfe0f4
Bengt Weiße <bengtler@gmail.com>
2022-09-22 15:13:04 +0200

    add logging

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ try {
   const jsonPackageFile = JSON.parse(fs.readFileSync(path.resolve(process.env.GITHUB_WORKSPACE, packagePath || 'package.json')))
   const regexp = new RegExp(`"name":"${jsonPackageFile.name}","version":".*?",`, 'g')
   const jsonLockFile = fs.readFileSync(path.resolve(process.env.GITHUB_WORKSPACE, lockPath || 'package-lock.json')).toString(('utf-8')).replace(/\n|\s|\r|\t/g, '').replace(regexp, '')
-
+  console.log(jsonLockFile.slice(0, 250))
   core.setOutput('cacheKey', crypto.createHash('sha256').update(jsonLockFile, 'utf8').digest('hex'));
 } catch (error) {
   core.setFailed(error.message);
