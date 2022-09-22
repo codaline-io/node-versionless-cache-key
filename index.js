dffc020b33e8c7c24a6394f7766fa0b9da26cc28
Bengt Weiße <bengtler@gmail.com>
2022-09-22 15:10:29 +0200

    -.-

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ try {
   const lockPath = core.getInput('packageLockJSON')
   const jsonPackageFile = JSON.parse(fs.readFileSync(path.resolve(process.env.GITHUB_WORKSPACE, packagePath || 'package.json')))
   const regexp = new RegExp(`"name":"${jsonPackageFile.name}","version":".*?",`, 'g')
-  const jsonLockFile = fs.readFileSync(path.resolve(process.env.GITHUB_WORKSPACE, lockPath || 'package-lock.json')).replace(/\n|\s|\r|\t/g, '').replace(regexp, '')
+  const jsonLockFile = fs.readFileSync(path.resolve(process.env.GITHUB_WORKSPACE, lockPath || 'package-lock.json')).toString(('utf-8')).replace(/\n|\s|\r|\t/g, '').replace(regexp, '')
 
   core.setOutput('cacheKey', crypto.createHash('sha256').update(jsonLockFile, 'utf8').digest('hex'));
 } catch (error) {
