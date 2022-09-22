c3abd17c4d8dbc477f2492e0973df1b94ea6828e
Bengt Weiße <bengtler@gmail.com>
2022-09-22 14:59:00 +0200

    fix: replace

--- index.js
+++ index.js
@@ -8,10 +8,7 @@ try {
   const lockPath = core.getInput('packageLockJSON')
   const jsonPackageFile = JSON.parse(fs.readFileSync(path.resolve(process.env.GITHUB_WORKSPACE, packagePath || 'package.json')))
   const regexp = new RegExp(`"name":"${jsonPackageFile.name}","version":".*?",`, 'g')
-  const jsonLockFile = JSON.parse(fs.readFileSync(path.resolve(process.env.GITHUB_WORKSPACE, lockPath || 'package-lock.json')))
-  .toString()
-  .replace(/\n|\s|\r|\t/g, '')
-  .replace(regexp, '')
+  const jsonLockFile = fs.readFileSync(path.resolve(process.env.GITHUB_WORKSPACE, lockPath || 'package-lock.json')).replace(/\n|\s|\r|\t/g, '').replace(regexp, '')
 
   core.setOutput('cacheKey', crypto.createHash('sha256').update(jsonLockFile, 'utf8').digest('hex'));
 } catch (error) {
